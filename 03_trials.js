d70c9cd06e66ae407de271ac12262210e5b79307
mrssoph <49271604+mrssoph@users.noreply.github.com>
2019-05-15 17:13:16 +0200

    Add files via upload

--- 03_trials.js
+++ 03_trials.js
@@ -1,19 +1,22 @@
 // In this file you can specify the trial data for your experiment
 
 
-const trial_info = {
-    forced_choice: [
-        {
-            question: "What's on the bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham'
-        },
-        {
-            question: "What's the weather like?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow"
-        }
-    ],
-};
+const key_press_trials = [
+    {
+        question: "Do you like cats?",
+        key1: 'y',
+        key2: 'n',
+        y: 'yes!',
+        n: 'no',
+        expected: 'yes!'
+    },
+    {
+        question: "Do you like dogs?",
+        picture: 'path/to/picture.jpg',
+        key1: 'y',
+        key2: 'n',
+        y: 'yes!',
+        n: 'no',
+        expected: 'yes!'
+    }
+];
