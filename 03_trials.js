c9eca34db49f3751cfb04f76d42ce60ad1be1aef
hgianotitone <henriquegianoti@gmail.com>
2019-05-15 17:44:22 +0200

    added pictures and 5 trials

--- 03_trials.js
+++ 03_trials.js
@@ -2,21 +2,51 @@
 
 
 const key_press_trials = [
-    {
-        question: "Do you like cats?",
+    {	
+		question: "Same or not?#1",
+		picture: C:\Users\Henrique\Desktop\Uni\Experimental Psychology\GitHub\XPLab_2019\homework3\mental_rotation_images\2_50_different,
         key1: 'y',
         key2: 'n',
         y: 'yes!',
         n: 'no',
-        expected: 'yes!'
+        expected: 'no'
+
     },
     {
-        question: "Do you like dogs?",
-        picture: 'path/to/picture.jpg',
+        question: "Same or not?#2",
+        picture: 'C:\Users\Henrique\Desktop\Uni\Experimental Psychology\GitHub\XPLab_2019\homework3\mental_rotation_images\6_150_same',
         key1: 'y',
         key2: 'n',
         y: 'yes!',
         n: 'no',
         expected: 'yes!'
     }
+	 {
+        question: "Same or not?#3",
+        picture: 'C:\Users\Henrique\Desktop\Uni\Experimental Psychology\GitHub\XPLab_2019\homework3\mental_rotation_images\8_50_same',
+        key1: 'y',
+        key2: 'n',
+        y: 'yes!',
+        n: 'no',
+        expected: 'yes!'
+    }
+	 {
+        question: "Same or not#4?",
+        picture: 'C:\Users\Henrique\Desktop\Uni\Experimental Psychology\GitHub\XPLab_2019\homework3\mental_rotation_images\11_150_same',
+        key1: 'y',
+        key2: 'n',
+        y: 'yes!',
+        n: 'no',
+        expected: 'yes!'
+    }
+	 {
+        question: "Same or not?#5",
+        picture: 'C:\Users\Henrique\Desktop\Uni\Experimental Psychology\GitHub\XPLab_2019\homework3\mental_rotation_images\9_150_different',
+        key1: 'y',
+        key2: 'n',
+        y: 'yes!',
+        n: 'no',
+        expected: 'no'
+    }
+	
 ];
